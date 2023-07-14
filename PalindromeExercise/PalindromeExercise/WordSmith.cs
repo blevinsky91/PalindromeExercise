ee9c7d8214a8c447e6da14f46887c97c0b14ae32
Brandon Levinsky <blevinsky91@gmail.com>
2023-07-14 15:12:19 -0400

    complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public static bool IsAPalindrome(string args)
+		{
+            
+            string input = new string(Array.FindAll(args.ToLower().ToCharArray(), char.IsLetterOrDigit));
+
+            
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true; 
+        }
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("sandwich", false)]
+        [InlineData("A man, a plan, a canal - Panama!", true)]
+        public void PalindromeTest(string args, bool expected)
         {
+            //Arrange
+            //Instantiate an object
 
+            WordSmith word = new WordSmith();
+
+            //Act
+            //Call method and assign to variable
+
+            var actual = WordSmith.IsAPalindrome(args);
+
+            //Assert
+            //Compare
+
+            Assert.Equal(expected, actual);
         }
     }
 }
